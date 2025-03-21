a606c2cbba47611df43c2ffe51f1befd0d1c7d9a
Idris FOUGHALI <idris.foughali@sfr.com>
2025-03-21 14:38:15 +0100

    Feat: reworked utility functions to iterate through cluster nodes

--- library.sh
+++ library.sh
@@ -67,182 +67,265 @@ EOF
 }
 
 
+
+
+
+install_go () {
+    CURRENT_NODE=$1
+    GO_VERSION=$2
+    TINYGO_VERSION=$3
+
+    ssh -q $CURRENT_NODE "
+        cd /tmp
+
+        # install Go
+        echo installing go version: ${GO_VERSION}
+        wget -q https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
+        sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+
+        # install tinyGo
+        wget -q https://github.com/tinygo-org/tinygo/releases/download/v${TINYGO_VERSION}/tinygo${TINYGO_VERSION}.linux-amd64.tar.gz
+        sudo tar -C /usr/local -xzf tinygo${TINYGO_VERSION}.linux-amd64.tar.gz
+
+
+        # Update path:
+        files=(
+            \"/etc/environment\"
+            \"\$HOME/.bashrc\"
+        )
+
+        extra_paths=(
+            \"/usr/local/go/bin\"
+            \"/usr/local/tinygo/bin\"
+        )
+
+        echo \"Updating paths for GO\"
+        for file in \"\${files[@]}\"; do
+            echo \"Updating environment for path: \${file}\"
+
+            for path in \"\${extra_paths[@]}\"; do
+                echo \"  Checking path: \$path\"
+
+                # Check if the export PATH line exists
+                if grep -q 'export PATH=' \"\$file\"; then
+                    # Ensure the path is not already appended
+                    if ! grep -q \"\$path\" \"\$file\"; then
+                        echo \"Appending \$path to \$file\"
+                        sudo sed -i \"s|^export PATH=.*|&:\${path}|\" \"\$file\"
+                    else
+                        echo \"\$path already exists in \$file\"
+                    fi
+                else
+                    echo \"export PATH not found, adding export line\"
+                    echo \"export PATH=\\\$PATH:\${path}\" | sudo tee -a \"\$file\" > /dev/null
+                fi
+            done
+        done
+
+    "
+}
+
+# if grep -q '^PATH=' \${file}; then
+#                     sudo sed -i "s\|^PATH=.*\|&:\${path}\|" \${file}
+#                 else
+#                     sudo sed -i "1i PATH=\$PATH:\${path}" \$file
+#                 fi
+
+
+# echo updating paths for GO
+# for file in "\${files[@]}"; do
+#     # update environemnt path:
+#     for path in "\${extra_paths[@]}"; do
+#         if grep -q '^PATH=' \${file}; then
+#             sudo sed -i "s|^PATH=.*|&:\${path}|" \${file}
+#         else
+#             sudo sed -i "1i PATH=\$PATH:\${path}" \$file
+#         fi
+#     done
+# done
+# echo Finished updating paths for GO
+
+
 update_firewall() {
 
-    # check zone:
-    if $(sudo firewall-cmd --get-zones | grep -q "k8s"); then
-        echo firewallD 'k8s' zone already exists
-    else
-        echo creating firewallD 'k8s' zone
-        sudo firewall-cmd --permanent --new-zone=k8s
-         # apply changes:
-        sudo firewall-cmd --reload
-    fi
-    # set k8s as default zone
-    sudo firewall-cmd --set-default-zone=k8s
-    #################################################################*
-    # allow http and k8s zones to use 22 and ICMP
+    CURRENT_NODE="$1"
 
-    # Open port 53 (DNS) for all networks (public zone)
-    sudo firewall-cmd --zone=public --add-service=dns --permanent
-    sudo firewall-cmd --zone=k8s --add-service=dns --permanent
+    ssh $CURRENT_NODE """
+        # check zone:
+        if \$\(sudo firewall-cmd --get-zones | grep -q "k8s"\); then
+            echo firewallD 'k8s' zone already exists
+        else
+            echo creating firewallD 'k8s' zone
+            sudo firewall-cmd --permanent --new-zone=k8s
+            # apply changes:
+            sudo firewall-cmd --reload
+        fi
+        # set k8s as default zone
+        sudo firewall-cmd --set-default-zone=k8s
+        # #################################################################*
+        # # allow http and k8s zones to use 22 and ICMP
 
-    sudo firewall-cmd --zone=public --add-port=53/tcp --permanent
-    sudo firewall-cmd --zone=public --add-port=53/udp --permanent
-    sudo firewall-cmd --zone=k8s --add-port=53/tcp --permanent
-    sudo firewall-cmd --zone=k8s --add-port=53/udp --permanent
+        # # Open port 53 (DNS) for all networks (public zone)
+        # sudo firewall-cmd --zone=public --add-service=dns --permanent
+        # sudo firewall-cmd --zone=k8s --add-service=dns --permanent
 
-
-    # Open port 22 (SSH) for all networks (public zone)
-    sudo firewall-cmd --zone=k8s --add-port=22/tcp --permanent
-    sudo firewall-cmd --zone=public --add-port=22/tcp --permanent
-
-    # Allow ICMP echo-reply/request  for all networks (public zone)
-    sudo firewall-cmd  --zone=k8s --permanent --add-icmp-block-inversion
-    sudo firewall-cmd  --zone=public --permanent --add-icmp-block-inversion
-
-    # Allow ICMP echo-reply (ping response) for all networks (public zone)
-    sudo firewall-cmd --permanent --zone=public --add-icmp-block=echo-reply
-    sudo firewall-cmd --permanent --zone=k8s --add-icmp-block=echo-reply
-
-    # Allow ICMP echo-request (ping) for all networks (public zone)
-    sudo firewall-cmd --permanent --zone=public --add-icmp-block=echo-request
-    sudo firewall-cmd --permanent --zone=k8s --add-icmp-block=echo-request
-
-    sudo firewall-cmd --reload
-    #############################################################################
-
-    # Kubectl API Server
-
-    sudo firewall-cmd --zone=public --add-port=${CONTROLPLANE_API_PORT}/tcp --permanent
-    sudo firewall-cmd --zone=k8s --add-port=${CONTROLPLANE_API_PORT}/tcp --permanent
-
-    # Kubelet health and communication
-    sudo firewall-cmd --zone=k8s --add-port=10248/tcp --permanent
-    sudo firewall-cmd --zone=k8s --add-port=10250/tcp --permanent
-
-    # Control plane services
-    sudo firewall-cmd --zone=k8s --add-port=10251/tcp --permanent  # Scheduler
-    sudo firewall-cmd --zone=k8s --add-port=10252/tcp --permanent  # Controller Manager
-    sudo firewall-cmd --zone=k8s --add-port=10257/tcp --permanent  # Secure Controller Manager
-    sudo firewall-cmd --zone=k8s --add-port=10259/tcp --permanent  # Secure Scheduler
+        # sudo firewall-cmd --zone=public --add-port=53/tcp --permanent
+        # sudo firewall-cmd --zone=public --add-port=53/udp --permanent
+        # sudo firewall-cmd --zone=k8s --add-port=53/tcp --permanent
+        # sudo firewall-cmd --zone=k8s --add-port=53/udp --permanent
 
 
-    sudo firewall-cmd --zone=k8s --add-port=4000/tcp --permanent
-    sudo firewall-cmd --zone=k8s --add-port=4245/tcp --permanent
-    sudo firewall-cmd --zone=k8s --add-port=443/tcp --permanent
-    sudo firewall-cmd --zone=k8s --add-port=80/tcp --permanent
-    sudo firewall-cmd --zone=k8s --add-port=8080/tcp --permanent
-    sudo firewall-cmd --zone=k8s --add-port=9090/tcp --permanent
+        # # Open port 22 (SSH) for all networks (public zone)
+        # sudo firewall-cmd --zone=k8s --add-port=22/tcp --permanent
+        # sudo firewall-cmd --zone=public --add-port=22/tcp --permanent
 
-    sudo firewall-cmd --zone=public --add-port=4000/tcp --permanent
-    sudo firewall-cmd --zone=public --add-port=4245/tcp --permanent
-    sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
-    sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
-    sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
-    sudo firewall-cmd --zone=public --add-port=9090/tcp --permanent
+        # # Allow ICMP echo-reply/request  for all networks (public zone)
+        # sudo firewall-cmd  --zone=k8s --permanent --add-icmp-block-inversion
+        # sudo firewall-cmd  --zone=public --permanent --add-icmp-block-inversion
 
-    # BGP for Calico/Cilium
-    sudo firewall-cmd --zone=k8s --add-port=179/tcp --permanent
+        # # Allow ICMP echo-reply (ping response) for all networks (public zone)
+        # sudo firewall-cmd --permanent --zone=public --add-icmp-block=echo-reply
+        # sudo firewall-cmd --permanent --zone=k8s --add-icmp-block=echo-reply
 
-    # etcd access
-    sudo firewall-cmd --zone=k8s --add-port=2379-2381/tcp --permanent
+        # # Allow ICMP echo-request (ping) for all networks (public zone)
+        # sudo firewall-cmd --permanent --zone=public --add-icmp-block=echo-request
+        # sudo firewall-cmd --permanent --zone=k8s --add-icmp-block=echo-request
 
-    # VXLAN overlay network communication (used for networking between nodes in Kubernetes with VXLAN encapsulation)
-    sudo firewall-cmd --zone=k8s --add-port=8472/udp --permanent
+        # sudo firewall-cmd --reload
+        # #############################################################################
 
-    # Health checks
-    sudo firewall-cmd --zone=k8s --add-port=4240/tcp --permanent
-    sudo firewall-cmd --zone=k8s --add-icmp-block=echo-request --permanent
+        # # Kubectl API Server
 
-    # Additional required ports for Cilium
-    sudo firewall-cmd --zone=k8s --add-port=4244/tcp --permanent  # Hubble server
-    sudo firewall-cmd --zone=k8s --add-port=4245/tcp --permanent  # Hubble Relay
-    sudo firewall-cmd --zone=k8s --add-port=4250/tcp --permanent  # Mutual Auth
-    sudo firewall-cmd --zone=k8s --add-port=51871/udp --permanent # WireGuard
+        # sudo firewall-cmd --zone=public --add-port=${CONTROLPLANE_API_PORT}/tcp --permanent
+        # sudo firewall-cmd --zone=k8s --add-port=${CONTROLPLANE_API_PORT}/tcp --permanent
 
-    # Spire Agent health check port (listening on 127.0.0.1 or ::1)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=4251/tcp
+        # # Kubelet health and communication
+        # sudo firewall-cmd --zone=k8s --add-port=10248/tcp --permanent
+        # sudo firewall-cmd --zone=k8s --add-port=10250/tcp --permanent
 
-    # cilium-agent pprof server (debugging, listening on 127.0.0.1)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=6060/tcp
-
-    # cilium-operator pprof server (debugging, listening on 127.0.0.1)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=6061/tcp
-
-    # Hubble Relay pprof server (debugging, listening on 127.0.0.1)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=6062/tcp
-
-    # cilium-envoy health listener (listening on 127.0.0.1)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=9878/tcp
-
-    # cilium-agent health status API (listening on 127.0.0.1 and/or ::1)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=9879/tcp
-
-    # cilium-agent gops server (debugging, listening on 127.0.0.1)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=9890/tcp
-
-    # cilium-operator gops server (debugging, listening on 127.0.0.1)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=9891/tcp
-
-    # Hubble Relay gops server (debugging, listening on 127.0.0.1)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=9893/tcp
-
-    # cilium-envoy Admin API (listening on 127.0.0.1)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=9901/tcp
-
-    # cilium-agent Prometheus metrics
-    sudo firewall-cmd --zone=k8s --permanent --add-port=9962/tcp
-
-    # cilium-operator Prometheus metrics
-    sudo firewall-cmd --zone=k8s --permanent --add-port=9963/tcp
-
-    # cilium-envoy Prometheus metrics
-    sudo firewall-cmd --zone=k8s --permanent --add-port=9964/tcp
-
-    # VXLAN overlay network communication (used by Cilium and other network plugins)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=4789/udp
-
-    # enable firewallD masquerade
-    sudo firewall-cmd --add-masquerade --permanent
+        # # Control plane services
+        # sudo firewall-cmd --zone=k8s --add-port=10251/tcp --permanent  # Scheduler
+        # sudo firewall-cmd --zone=k8s --add-port=10252/tcp --permanent  # Controller Manager
+        # sudo firewall-cmd --zone=k8s --add-port=10257/tcp --permanent  # Secure Controller Manager
+        # sudo firewall-cmd --zone=k8s --add-port=10259/tcp --permanent  # Secure Scheduler
 
 
-    #############################################################################
-    # worker nodes ports:
-    # VXLAN overlay network communication (used for networking between nodes in Kubernetes with VXLAN encapsulation)
-    sudo firewall-cmd --zone=k8s --add-port=8472/udp --permanent
+        # sudo firewall-cmd --zone=k8s --add-port=4000/tcp --permanent
+        # sudo firewall-cmd --zone=k8s --add-port=4245/tcp --permanent
+        # sudo firewall-cmd --zone=k8s --add-port=443/tcp --permanent
+        # sudo firewall-cmd --zone=k8s --add-port=80/tcp --permanent
+        # sudo firewall-cmd --zone=k8s --add-port=8080/tcp --permanent
+        # sudo firewall-cmd --zone=k8s --add-port=9090/tcp --permanent
 
-    # Health check port for cluster status (Cilium health checks and similar services)
-    sudo firewall-cmd --zone=k8s --add-port=4240/tcp --permanent
+        # sudo firewall-cmd --zone=public --add-port=4000/tcp --permanent
+        # sudo firewall-cmd --zone=public --add-port=4245/tcp --permanent
+        # sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
+        # sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+        # sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
+        # sudo firewall-cmd --zone=public --add-port=9090/tcp --permanent
 
-    # ICMP echo-request (ping) allowed for health checks and diagnostics
-    sudo firewall-cmd --zone=k8s --add-icmp-block=echo-request --permanent
+        # # BGP for Calico/Cilium
+        # sudo firewall-cmd --zone=k8s --add-port=179/tcp --permanent
 
-    # Access to etcd cluster (used for the Kubernetes control plane communication)
-    sudo firewall-cmd --zone=k8s --add-port=2379-2380/tcp --permanent
+        # # etcd access
+        # sudo firewall-cmd --zone=k8s --add-port=2379-2381/tcp --permanent
 
-    # Kubernetes node ports, including worker and master services and external access (for services like kubelet and external services)
-    sudo firewall-cmd --zone=k8s --add-port=30000-32767/tcp --permanent
-    sudo firewall-cmd --zone=public --add-port=30000-32767/tcp --permanent
-    sudo firewall-cmd --reload
+        # # VXLAN overlay network communication (used for networking between nodes in Kubernetes with VXLAN encapsulation)
+        # sudo firewall-cmd --zone=k8s --add-port=8472/udp --permanent
 
-    # Kubelet health and communication
-    sudo firewall-cmd --zone=k8s --add-port=10250/tcp --permanent
+        # # Health checks
+        # sudo firewall-cmd --zone=k8s --add-port=4240/tcp --permanent
+        # sudo firewall-cmd --zone=k8s --add-icmp-block=echo-request --permanent
 
-    # BGP for Calico/Cilium
-    sudo firewall-cmd --zone=k8s --add-port=179/tcp --permanent
+        # # Additional required ports for Cilium
+        # sudo firewall-cmd --zone=k8s --add-port=4244/tcp --permanent  # Hubble server
+        # sudo firewall-cmd --zone=k8s --add-port=4245/tcp --permanent  # Hubble Relay
+        # sudo firewall-cmd --zone=k8s --add-port=4250/tcp --permanent  # Mutual Auth
+        # sudo firewall-cmd --zone=k8s --add-port=51871/udp --permanent # WireGuard
 
-    # Additional required ports for Cilium
-    sudo firewall-cmd --zone=k8s --add-port=4244/tcp --permanent  # Hubble server
-    sudo firewall-cmd --zone=k8s --add-port=4245/tcp --permanent  # Hubble Relay
-    sudo firewall-cmd --zone=k8s --add-port=4250/tcp --permanent  # Mutual Auth
-    sudo firewall-cmd --zone=k8s --add-port=51871/udp --permanent # WireGuard
+        # # Spire Agent health check port (listening on 127.0.0.1 or ::1)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=4251/tcp
 
-    # VXLAN overlay network communication (used for networking between nodes in Kubernetes with VXLAN encapsulation)
-    sudo firewall-cmd --zone=k8s --permanent --add-port=4789/udp
-    #############################################################################
-    # firewall-cmd --list-all
-    sudo firewall-cmd --reload
+        # # cilium-agent pprof server (debugging, listening on 127.0.0.1)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=6060/tcp
+
+        # # cilium-operator pprof server (debugging, listening on 127.0.0.1)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=6061/tcp
+
+        # # Hubble Relay pprof server (debugging, listening on 127.0.0.1)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=6062/tcp
+
+        # # cilium-envoy health listener (listening on 127.0.0.1)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=9878/tcp
+
+        # # cilium-agent health status API (listening on 127.0.0.1 and/or ::1)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=9879/tcp
+
+        # # cilium-agent gops server (debugging, listening on 127.0.0.1)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=9890/tcp
+
+        # # cilium-operator gops server (debugging, listening on 127.0.0.1)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=9891/tcp
+
+        # # Hubble Relay gops server (debugging, listening on 127.0.0.1)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=9893/tcp
+
+        # # cilium-envoy Admin API (listening on 127.0.0.1)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=9901/tcp
+
+        # # cilium-agent Prometheus metrics
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=9962/tcp
+
+        # # cilium-operator Prometheus metrics
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=9963/tcp
+
+        # # cilium-envoy Prometheus metrics
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=9964/tcp
+
+        # # VXLAN overlay network communication (used by Cilium and other network plugins)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=4789/udp
+
+        # # enable firewallD masquerade
+        # sudo firewall-cmd --add-masquerade --permanent
+
+
+        # #############################################################################
+        # # worker nodes ports:
+        # # VXLAN overlay network communication (used for networking between nodes in Kubernetes with VXLAN encapsulation)
+        # sudo firewall-cmd --zone=k8s --add-port=8472/udp --permanent
+
+        # # Health check port for cluster status (Cilium health checks and similar services)
+        # sudo firewall-cmd --zone=k8s --add-port=4240/tcp --permanent
+
+        # # ICMP echo-request (ping) allowed for health checks and diagnostics
+        # sudo firewall-cmd --zone=k8s --add-icmp-block=echo-request --permanent
+
+        # # Access to etcd cluster (used for the Kubernetes control plane communication)
+        # sudo firewall-cmd --zone=k8s --add-port=2379-2380/tcp --permanent
+
+        # # Kubernetes node ports, including worker and master services and external access (for services like kubelet and external services)
+        # sudo firewall-cmd --zone=k8s --add-port=30000-32767/tcp --permanent
+        # sudo firewall-cmd --zone=public --add-port=30000-32767/tcp --permanent
+        # sudo firewall-cmd --reload
+
+        # # Kubelet health and communication
+        # sudo firewall-cmd --zone=k8s --add-port=10250/tcp --permanent
+
+        # # BGP for Calico/Cilium
+        # sudo firewall-cmd --zone=k8s --add-port=179/tcp --permanent
+
+        # # Additional required ports for Cilium
+        # sudo firewall-cmd --zone=k8s --add-port=4244/tcp --permanent  # Hubble server
+        # sudo firewall-cmd --zone=k8s --add-port=4245/tcp --permanent  # Hubble Relay
+        # sudo firewall-cmd --zone=k8s --add-port=4250/tcp --permanent  # Mutual Auth
+        # sudo firewall-cmd --zone=k8s --add-port=51871/udp --permanent # WireGuard
+
+        # # VXLAN overlay network communication (used for networking between nodes in Kubernetes with VXLAN encapsulation)
+        # sudo firewall-cmd --zone=k8s --permanent --add-port=4789/udp
+        # #############################################################################
+        # # firewall-cmd --list-all
+        # sudo firewall-cmd --reload
+    """
 }
 
 
